41827935ded7a41a4e3b990d49698917c4bddcdb
PeepaDylan <xugaohero@163.com>
2020-03-26 17:49:20 -0700

    update

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearch{
     public static void main(String[] args){
         int arr[] = {1, 8, 10, 89, 1000, 1234};
+        int arr2[] = {1, 8, 10, 89, 1000, 1000, 1000, 1000, 1234};
 
-        int resIndex = binarySearch(arr, 0, arr.length - 1, 1234);
-        System.out.println(resIndex);
+        // int resIndex = binarySearch(arr, 0, arr.length - 1, 1234);
+        List<Integer> resIndexList = binarySearch2(arr2, 0, arr2.length - 1, 1000);
+        System.out.println(resIndexList);
     }
 
     public static int binarySearch(int[] arr, int left, int right, int findVal){
@@ -22,5 +27,41 @@ public class BinarySearch{
             return mid;
         }
        // return -1;
+    }
+
+    public static ArrayList<Integer> binarySearch2(int[] arr, int left, int right, int findVal){
+        // 当left > right时，说明递归完了整个数组
+        if(left > right){
+            return new ArrayList<Integer>();
+        }
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if(findVal > midVal){
+            return binarySearch2(arr, mid + 1, right, findVal);
+        }else if(findVal < midVal){
+            return binarySearch2(arr, left, mid - 1, findVal);
+        }else{
+            ArrayList<Integer> resIndexlist = new ArrayList<Integer>();
+            int temp = mid - 1;
+            while(true){
+                if(temp < 0 || arr[temp]!= findVal){
+                    break;
+                }
+                resIndexlist.add(temp);
+                temp -= 1;
+            }
+            resIndexlist.add(mid);
+            
+            temp = mid + 1;
+            while(true){
+                if(temp < 0 || arr[temp]!= findVal){
+                    break;
+                }
+                resIndexlist.add(temp);
+                temp += 1;
+            }
+            return resIndexlist;
+        }
     }
 }
