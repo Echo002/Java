a8789911bcd79a1877289e86c80204b6d518e1a9
Echo002 <xugaohero@163.com>
2020-05-10 18:04:32 -0700

    update

---
+++ tree/huffmancode/HuffmanCode.java
@@ -0,0 +1,188 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.lang.Integer;
+
+public class HuffmanCode {
+    public static void main(String[] args) {
+        String content = "i like like like java do you like a java";
+        byte[] contentBytes = content.getBytes();
+        // System.out.println(contentByte.length); 40
+
+        List<Node> nodes = getNodes(contentBytes);
+        System.out.println(nodes);
+
+        // 测试——创建二叉树
+        Node huffmanTreeRoot = createHuffmanTree(nodes);
+        System.out.println("前序遍历");
+        huffmanTreeRoot.preOrder();
+
+        // 测试——创建对应的Huffman编码
+        // getCodes(huffmanTreeRoot, "", stringBuilder);
+        Map<Byte, String> huffmanCodes = getCodes(huffmanTreeRoot);
+        System.out.println("HuffmanCodeList:" + huffmanCodes);
+
+        // 测试
+        zip(contentBytes, huffmanCodes);
+    }
+
+    // 将字符数组通过Huffman编码表编码之后的Huffman编码
+    /*
+    bytes: 原始字符串对应的byte[]
+    huffmanCodes: 生成的huffman编码map
+    */
+    private static Byte[] zip(byte[] bytes, Map<Byte, String> huffmanCodes){
+        // 1. 利用huffmanCodes将bytes转成Huffman编码对应的字符串
+        StringBuilder stringbuilder = new StringBuilder();
+        // 遍历bytes数组
+        for(byte b:bytes){
+            stringBuilder.append(huffmanCodes.get(b));
+        }
+        // 统计返回的Huffman编码长度
+        int len;
+        if(stringBuilder.length() % 8 == 0){
+            len = stringbuilder.length() / 8;
+        }else{
+            len = stringbuilder.length() / 8 + 1; 
+        }
+        // int len = (stringbuilder.length() + 7) / 8;
+
+        // 创建存储压缩后的byte数组
+        byte[] huffmanCodeBytes = new byte[len];
+        int index;
+        for(int i = 0; i < stringbuilder.length(); i += 8){ // 每8位对应一个byte
+            String strByte;
+            strByte = stringbuilder.substring(i, i + 8);
+            // 将strByte转化成一个byte放入
+            
+        }
+    }
+
+    // 生成Huffman树对应的Huffman编码
+    /*
+    1. 将Huffman编码表存放在Map<Byte, String>形式 ASCII->编码
+    2. 生成编码表时，需要拼接路径，定义一个StringBuilder存储叶子节点的路径
+     */
+    static Map<Byte, String> huffmanCodes = new HashMap<Byte, String>();
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    // 为了调用方便我们重载getCodes
+    private static Map<Byte, String> getCodes(Node root){
+        if(root == null){
+            return null;
+        }
+        // 处理root
+        getCodes(root.left, "0", stringBuilder);
+        getCodes(root.right, "1", stringBuilder);
+        return huffmanCodes;
+    }
+
+    // 将传入的node节点的所有叶子节点的Huffman编码得到并放入到HuffmanCodes集合中
+    // node为传入节点 code为路径(左0右1)
+    private static void getCodes(Node node, String code, StringBuilder stringBuilder){
+        StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
+        // 将code加入到stringBuilder2中
+        stringBuilder2.append(code);
+        if(node != null){       // 如果node == null不处理
+            // 判断当前节点是否为叶子节点
+            if(node.data == null){  // 非叶子节点
+                // 递归处理
+                getCodes(node.left, "0", stringBuilder2);
+                getCodes(node.right, "1", stringBuilder2);
+            }else{                  // 叶子节点
+                huffmanCodes.put(node.data, stringBuilder2.toString());
+            }
+        }
+    }
+
+
+    // 前序遍历
+    private static void preOrder(Node root){
+        if(root != null){
+            root.preOrder();
+        }else{
+            System.out.println("Huffman树为空");
+        }
+    }
+
+    // 接收字节数组，返回List
+    private static List<Node> getNodes(byte[] bytes){
+        // 1 创建一个ArrayList
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        // 遍历bytes，统计每一个byte出现的次数 map[key, value]
+        Map<Byte,Integer> counts = new HashMap<>();
+        for(byte b:bytes){
+            Integer count = counts.get(b);
+            // map没有字符数据
+            if(count == null){
+                counts.put(b, 1);
+            }else{
+                counts.put(b, count + 1);
+            }
+        }
+
+        // 键值对转换成Node对象并加入到Nodes集合
+        for(Map.Entry<Byte, Integer> entry: counts.entrySet()){
+            nodes.add(new Node(entry.getKey(), entry.getValue()));
+        }
+        return nodes;
+    }
+
+    public static Node createHuffmanTree(List<Node> nodes){
+        while(nodes.size() > 1){
+            // 排序(从小到大)
+            Collections.sort(nodes);
+            // 取出第一颗最小的二叉树
+            Node leftNode = nodes.get(0);
+            // 取出第二颗最小的二叉树
+            Node rightNode = nodes.get(1);
+            // 创建一颗新的二叉树，根节点没有data只有权值
+            Node parent = new Node(null, leftNode.weight + rightNode.weight);
+            parent.left = leftNode;
+            parent.right = rightNode;
+
+            // 将已经处理的二叉树移除
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+            // 将新的二叉树加入
+            nodes.add(parent);
+        }
+        // 返回最后的节点
+        return nodes.get(0);
+    }
+}
+
+// 节点
+class Node implements Comparable<Node>{
+    Byte data;      // 数据本身
+    int weight;     // 权值
+    Node left;
+    Node right;
+
+    public Node(Byte data, int weight){
+        this.data = data;
+        this.weight = weight;
+    }
+    @Override
+    public int compareTo(Node o){
+        // 从小到大排序
+        return this.weight - o.weight;
+    }
+
+    public String toString(){
+        return "Node [data = " + data + ",weight =" + weight + "]";
+    }
+
+    // 前序遍历
+    public void preOrder(){
+        System.out.println(this);
+        if(this.left != null){
+            this.left.preOrder();
+        }
+        if(this.right != null){
+            this.right.preOrder();
+        }
+    }
+}
--- tree/huffmantree/HuffmanTree.java
+++ tree/huffmantree/HuffmanTree.java
@@ -5,10 +5,22 @@ import java.util.List;
 public class HuffmanTree {
     public static void main(String[] args) {
         int arr[] = {13, 7, 8, 3, 29, 6, 1};
-        createHuffmanTree(arr);
+        Node root = createHuffmanTree(arr);
+
+        // 测试
+        preOrder(root);
     }
 
-    public static void createHuffmanTree(int[] arr){
+    // 前序遍历
+    public static void preOrder(Node root){
+        if(root != null){
+            root.preOrder();
+        }else{
+            System.out.println("是空树，不能遍历");
+        }
+    }
+
+    public static Node createHuffmanTree(int[] arr){
         // 第一步，为了操作方便
         // 1.遍历arr数组
         // 2.将arr的每个元素构成成一个Node
@@ -18,30 +30,31 @@ public class HuffmanTree {
             nodes.add(new Node(value));
         }
 
-        
-        // 排序（从小到大）
-        Collections.sort(nodes);
-        System.out.println("nodes = " + nodes);
+        while(nodes.size() > 1){
+            // 排序（从小到大）
+            Collections.sort(nodes);
+            System.out.println("nodes = " + nodes);
 
-        // 取出根节点权值最小的两棵二叉树
-        // (1)取出权值最小的节点(二叉树)
-        Node leftNode = nodes.get(0);
-        // (2)取出权值第二小的节点(二叉树)
-        Node rightNode = nodes.get(1);
+            // 取出根节点权值最小的两棵二叉树
+            // (1)取出权值最小的节点(二叉树)
+            Node leftNode = nodes.get(0);
+            // (2)取出权值第二小的节点(二叉树)
+            Node rightNode = nodes.get(1);
 
-        // (3)构建一课新的二叉树
-        Node parent = new Node(leftNode.value + rightNode.value);
-        parent.left = leftNode;
-        parent.right = rightNode;
+            // (3)构建一课新的二叉树
+            Node parent = new Node(leftNode.value + rightNode.value);
+            parent.left = leftNode;
+            parent.right = rightNode;
 
-        // (4)从ArrayList删除处理过的二叉树
-        nodes.remove(leftNode);
-        nodes.remove(rightNode);
+            // (4)从ArrayList删除处理过的二叉树
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
 
-        // (5)将parent加入到nodes
-        nodes.add(parent);
-
-        System.out.println("第一次处理后" + nodes);
+            // (5)将parent加入到nodes
+            nodes.add(parent);
+        }
+        //返回
+        return nodes.get(0);
     }
 }
 
@@ -51,6 +64,17 @@ class Node implements Comparable<Node>{
     int value;      // 权值
     Node left;      // 指向左子节点
     Node right;     // 指向右子节点
+
+    // 前序遍历
+    public void preOrder(){
+        System.out.println(this);
+        if(this.left != null){
+            this.left.preOrder();
+        }
+        if(this.right != null){
+            this.right.preOrder();
+        }
+    }
 
     public Node(int value){
         this.value = value;
